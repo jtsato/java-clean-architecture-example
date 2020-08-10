9d1eba74b493271751b79e88b105584d67f83c64
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2020-08-10 11:19:06 -0300

    Remove imports

--- entrypoints/rest/src/main/java/io/github/jtsato/bookstore/entrypoint/rest/enumerator/mapper/SearchEnumeratorsPresenter.java
+++ entrypoints/rest/src/main/java/io/github/jtsato/bookstore/entrypoint/rest/enumerator/mapper/SearchEnumeratorsPresenter.java
@@ -1,10 +1,8 @@
 package io.github.jtsato.bookstore.entrypoint.rest.enumerator.mapper;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
