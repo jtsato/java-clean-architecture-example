a1da362b2b705d782ab114e73fd3e568d76e0f4a
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2020-08-18 22:59:55 -0300

    Refactor

--- configuration/src/main/java/io/github/jtsato/bookstore/configuration/SpringSecurityConfiguration.java
+++ configuration/src/main/java/io/github/jtsato/bookstore/configuration/SpringSecurityConfiguration.java
@@ -3,7 +3,6 @@ package io.github.jtsato.bookstore.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -20,9 +19,9 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity httpSecurity)
         throws Exception {
         
-        //httpSecurity.authorizeRequests().anyRequest().authenticated().and().oauth2Login().and().oauth2ResourceServer().jwt();
-        //httpSecurity.requiresChannel().requestMatchers(request -> request.getHeader("X-Forwarded-Proto") != null).requiresSecure();
-        //httpSecurity.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-        //httpSecurity.headers().contentSecurityPolicy("script-src 'self'; report-to /csp-report-endpoint/");
+        httpSecurity.authorizeRequests().anyRequest().authenticated().and().oauth2Login().and().oauth2ResourceServer().jwt();
+        httpSecurity.requiresChannel().requestMatchers(request -> request.getHeader("X-Forwarded-Proto") != null).requiresSecure();
+        httpSecurity.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        httpSecurity.headers().contentSecurityPolicy("script-src 'self'; report-to /csp-report-endpoint/");
     }
 }
--- configuration/src/main/java/io/github/jtsato/bookstore/configuration/SwaggerUIConfiguration.java
+++ configuration/src/main/java/io/github/jtsato/bookstore/configuration/SwaggerUIConfiguration.java
@@ -3,6 +3,7 @@ package io.github.jtsato.bookstore.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
