82afca9a1a1f006c1d1cf25580700a4efaff335e
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2020-08-29 01:05:47 -0300

    Fix  401 and 403 on Okta

--- configuration/src/main/java/io/github/jtsato/bookstore/configuration/SpringSecurityConfiguration.java
+++ configuration/src/main/java/io/github/jtsato/bookstore/configuration/SpringSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import com.okta.spring.boot.oauth.Okta;
@@ -20,7 +21,13 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity httpSecurity)
         throws Exception {
-
+        httpSecurity.authorizeRequests().anyRequest().authenticated().and().oauth2ResourceServer().jwt();
         Okta.configureResourceServer401ResponseBody(httpSecurity);
+    }
+
+    @Override
+    public void configure(final WebSecurity web)
+        throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
     }
 }
